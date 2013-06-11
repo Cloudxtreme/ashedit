c56ba6586f503e10077347bb762313cab670455f
Trent Gamblin <trent@nooskewl.com>
2013-06-11 15:58:16 -0600

    Add MO2 cmake option

--- general.cpp
+++ general.cpp
@@ -2,17 +2,8 @@
 
 int General::areaSize = 20;
 
-#define MONSTERRPG2
-
-#ifdef MONSTERRPG2
 int General::tileSize = 16;
 int General::startLayers = 4;
 bool General::can_add_and_delete_layers = true;
 int General::scale = 2;
-#else
-int General::tileSize = 64;
-int General::startLayers = 2;
-bool General::can_add_and_delete_layers = true;
-int General::scale = 1;
-#endif
 
